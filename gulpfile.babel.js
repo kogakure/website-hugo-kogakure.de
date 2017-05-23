8fe4e316f24f5cdead60bb0f69aaca129c1ec66d
Stefan Imhoff <stefan@imhoff.name>
2017-05-23 20:10:14 +0200

    Create Gulp task to create Hugo site

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,24 @@
 import gulp from 'gulp';
+import cp from 'child_process';
 import BrowserSync from 'browser-sync';
 
 const browserSync = BrowserSync.create();
+const hugoBin = 'hugo';
+const defaultArgs = ['--config=config.toml'];
+
+function buildSite(callback, options) {
+  const args = options ? defaultArgs.concat(options) : defaultArgs;
+
+  return cp.spawn(hugoBin, args, { stdio: 'inherit' }).on('close', (code) => {
+    if (code === 0) {
+      browserSync.reload();
+      callback();
+    } else {
+      browserSync.notify('Hugo build failed');
+      callback('Hugo build failed');
+    }
+  });
+}
 
 gulp.task('server', () =>{
   browserSync.init({
@@ -14,3 +31,6 @@ gulp.task('server', () =>{
     }
   });
 });
+
+gulp.task('hugo', (callback) => buildSite(callback));
+
