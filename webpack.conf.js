c361f28f8d24ed90fb6b2c231bf7157c60d49f25
Stefan Imhoff <stefan@imhoff.name>
2018-03-03 12:41:01 +0100

    chore(npm): Upgrade npm packages

--- webpack.conf.js
+++ webpack.conf.js
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
+  mode: 'development',
   module: {
     rules: [
       {
@@ -22,24 +23,22 @@ export default {
       }
     ]
   },
-
+  optimization: {
+    minimize: false
+  },
   plugins: [
     new webpack.ProvidePlugin({
-      'fetch': 'imports-loader?this=>global!exports?global.fetch!whatwg-fetch'
+      fetch: 'imports-loader?this=>global!exports?global.fetch!whatwg-fetch'
     })
   ],
-
   context: path.join(__dirname, 'src'),
-
   entry: {
     app: ['./js/app']
   },
-
   output: {
     path: path.join(__dirname, 'dist/assets/js/'),
     publicPath: '/',
     filename: '[name].js'
   },
-
   externals: [/^vendor\/.+\.js$/]
 };
