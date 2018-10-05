761bdb066e9b1529172a16f0c322895288d0693b
Stefan Imhoff <stefan@imhoff.name>
2018-10-05 16:49:22 +0200

    Remove Google Analytics tracking

--- app/content/impressum.md
+++ app/content/impressum.md
@@ -53,13 +53,6 @@ Sie haben jederzeit das Recht auf Auskunft über die bezüglich Ihrer Person ges
 
   [Impressum]: /impressum/
 
-### Daten zur Webanalyse
-
-Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. (Google). Google Analytics verwendet sog. Cookies, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre Benutzung diese Website (einschließlich Ihrer IP-Adresse) wird an einen Server der Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden. Bitte informieren Sie sich diesbezüglich unter den [Allgemeinen Geschäftsbedingungen für Google Analytics]. Bitte berücksichtigen Sie zusätzlich [Googles Datenschutzerklärung].
-
-  [Allgemeinen Geschäftsbedingungen für Google Analytics]: http://www.google.com/analytics/terms/de.html
-  [Googles Datenschutzerklärung]: http://www.google.com/intl/de/policies/privacy/
-
 ### Weitere Informationen
 
 Ihr Vertrauen ist mir wichtig. Daher möchte ich Ihnen jederzeit Rede und Antwort bezüglich der Verarbeitung Ihrer personenbezogenen Daten stehen. Wenn Sie Fragen haben, die Ihnen diese Datenschutzerklärung nicht beantworten konnte oder wenn Sie zu einem Punkt vertiefte Informationen wünschen, wenden Sie sich bitte jederzeit an mich.
--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,23 @@
-import fitvids from 'fitvids';
+import fitvids from "fitvids";
 
-import navigation from './lib/navigation';
-import figure from './lib/figure';
-import tracking from './lib/tracking';
-
-const trackingLinks = document.querySelectorAll('a');
+import navigation from "./lib/navigation";
+import figure from "./lib/figure";
 
 // https://github.com/google/web-starter-kit/
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    window.location.hostname === '[::1]' ||
+  window.location.hostname === "localhost" ||
+    window.location.hostname === "[::1]" ||
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-    ),
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 );
 
 if (
-  'serviceWorker' in navigator &&
-  (window.location.protocol === 'https:' || isLocalhost)
+  "serviceWorker" in navigator &&
+  (window.location.protocol === "https:" || isLocalhost)
 ) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register("/service-worker.js")
     .then(function(registration) {
       registration.onupdatefound = function() {
         if (navigator.serviceWorker.controller) {
@@ -28,12 +25,12 @@ if (
 
           installingWorker.onstatechange = function() {
             switch (installingWorker.state) {
-              case 'installed':
+              case "installed":
                 break;
 
-              case 'redundant':
+              case "redundant":
                 throw new Error(
-                  'The installing ' + 'service worker became redundant.',
+                  "The installing " + "service worker became redundant."
                 );
 
               default:
@@ -44,31 +41,21 @@ if (
       };
     })
     .catch(function(e) {
-      console.error('Error during service worker registration:', e); // eslint-disable-line no-console
+      console.error("Error during service worker registration:", e); // eslint-disable-line no-console
     });
 }
 
-if ('querySelector' in document && 'addEventListener' in window) {
-  document.addEventListener('DOMContentLoaded', () => {
+if ("querySelector" in document && "addEventListener" in window) {
+  document.addEventListener("DOMContentLoaded", () => {
     // Fitvids
-    fitvids('.container');
+    fitvids(".container");
 
     // Highlighting of navigation item
     if (navigation.currentNavigationItem()) {
-      navigation.currentNavigationItem().classList.add('is-active');
+      navigation.currentNavigationItem().classList.add("is-active");
     }
 
     // Figurize
     figure.figurizeImage();
-
-    // Tracking
-    for (let i = 0, len = trackingLinks.length; i < len; i++) {
-      const trackingLink = trackingLinks[i];
-
-      trackingLink.addEventListener(
-        'click',
-        tracking.trackLinksWithGoogleAnalytics,
-      );
-    }
   });
 }
