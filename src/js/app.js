81f057aa8af1ae508f6e2fdf3112be779fa78519
Stefan Imhoff <stefan@imhoff.name>
2018-10-05 16:53:25 +0200

    Remove old code and run prettier

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,18 @@
-import fitvids from "fitvids";
+import fitvids from 'fitvids';
 
-import navigation from "./lib/navigation";
-import figure from "./lib/figure";
+import navigation from './lib/navigation';
+import figure from './lib/figure';
 
 // https://github.com/google/web-starter-kit/
 const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    window.location.hostname === "[::1]" ||
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
-if (
-  "serviceWorker" in navigator &&
-  (window.location.protocol === "https:" || isLocalhost)
-) {
+if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)) {
   navigator.serviceWorker
-    .register("/service-worker.js")
+    .register('/service-worker.js')
     .then(function(registration) {
       registration.onupdatefound = function() {
         if (navigator.serviceWorker.controller) {
@@ -25,13 +20,11 @@ if (
 
           installingWorker.onstatechange = function() {
             switch (installingWorker.state) {
-              case "installed":
+              case 'installed':
                 break;
 
-              case "redundant":
-                throw new Error(
-                  "The installing " + "service worker became redundant."
-                );
+              case 'redundant':
+                throw new Error('The installing ' + 'service worker became redundant.');
 
               default:
               // Ignore
@@ -41,18 +34,18 @@ if (
       };
     })
     .catch(function(e) {
-      console.error("Error during service worker registration:", e); // eslint-disable-line no-console
+      console.error('Error during service worker registration:', e); // eslint-disable-line no-console
     });
 }
 
-if ("querySelector" in document && "addEventListener" in window) {
-  document.addEventListener("DOMContentLoaded", () => {
+if ('querySelector' in document && 'addEventListener' in window) {
+  document.addEventListener('DOMContentLoaded', () => {
     // Fitvids
-    fitvids(".container");
+    fitvids('.container');
 
     // Highlighting of navigation item
     if (navigation.currentNavigationItem()) {
-      navigation.currentNavigationItem().classList.add("is-active");
+      navigation.currentNavigationItem().classList.add('is-active');
     }
 
     // Figurize
